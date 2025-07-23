26f8719a42da1de4808a052d38a4eed49c1b490e
Isaac Okusu <isaacokusu66@gmail.com>
2025-07-23 14:40:33 +0000

    no significant changes

--- src/app/api/vapi/generate/route.ts
+++ src/app/api/vapi/generate/route.ts
@@ -1,9 +1,7 @@
-
-import { generateText } from "ai";
-import { google } from "@ai-sdk/google";
-
-import { db } from "@/firebase/admin";
-import { getRandomInterviewCover } from "@/lib/utils";
+import {generateText} from 'ai'
+import {google} from '@ai-sdk/google'
+import { getRandomInterviewCover } from '../../../../../lib/utils';
+import { db } from '../../../../../firebase/admin';
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
